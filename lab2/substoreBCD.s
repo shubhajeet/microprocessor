1a0f2dd22bcf47b4fe2db72d74f804aa4a948b69
sujit <mail@maharjansujit.com.np>
2015-07-13 22:22:45 +0545

    subtract 16 bit data implemented

---
+++ lab2/substoreBCD.s
@@ -0,0 +1,20 @@
+        ;; Using the same data of problem 15 write a program to subtract BCD data at 4286H form BCD data at 4284H data at 4287H and store the BCD result at 4288H
+        ;; loading data
+        STA     4584H
+        MOV     C, A
+        STA     4586H
+        SUA     C
+        ;; converting to BCD
+        DAA
+        ;; storing result
+        LDA     4588H
+        ;; loading data
+        STA     4585H
+        MOV     B, A
+        STA     4587H
+        SUC     B
+        ;; converting to BCD
+        DAA
+        ; storing result
+        LDA     4589H
+        HLT

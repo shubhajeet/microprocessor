9680da843244f360a6c7a4f43f76d55224ef4f29
sujit <mail@maharjansujit.com.np>
2015-07-13 20:58:54 +0545

    assigment 2 subtraction

---
+++ lab2/sub.s
@@ -0,0 +1,11 @@
+        ;; Move the data 22H and 48H to accumulator and register B respectively. Write a program to subtarct the register B form accumulator and store the result at register C. Again swap the accumulator content and register B content and subtra the register B content from accumulator content. See the flag content and the register content before and after the execution.
+        ;; loading data
+        MVI     B, 48H
+        MVI     A, 22H
+        ;; subtracting with b
+        SUB     B
+        ;; moving result to c
+        MOV     C, A
+        ;; subtracting with b
+        SUB     B
+        HLT

1b93d538d27b79746cb88cdafcc15b5e234461ed
Sujit Maharjan <mail@maharjansujit.com.np>
2015-07-16 21:24:47 +0545

    assignment file creation

---
+++ lab3/setAYandreset0.s
@@ -0,0 +1,3 @@
+        ;; Write a program to set auxiliary flag and reset parity flag without affecting other flags.
+
+        RST 5
---
+++ lab3/setZPnresetO.s
@@ -0,0 +1,3 @@
+        ;; Write a program to set zero and parity flags and reset other flags
+        MVI A, 00
+        RST 5

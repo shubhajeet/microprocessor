8581d681d7b46eef601b53bfb71d2c70050ae47e
Sujit Maharjan <mail@maharjansujit.com.np>
2015-07-16 21:10:08 +0545

    or two ram data

--- lab3/xortwomem.s
+++
@@ -1,8 +0,0 @@
-        ;; Write a program to XOR the content of 9027 with the content of location 9028H and store the content of 9029H
-        LXI H, 9027H
-        MOV A, M
-        INX H
-        XRA M
-        INX
-        MOV M, A
-        RST 5

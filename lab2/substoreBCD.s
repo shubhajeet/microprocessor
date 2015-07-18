44f3a568337cf01a12e4a1fef23c3f640406cbc6
sujit <mail@maharjansujit.com.np>
2015-07-18 15:42:47 +0545

    removing STA LDA confusion

--- lab2/substoreBCD.s
+++ lab2/substoreBCD.s
@@ -1,20 +1,20 @@
         ;; Using the same data of problem 15 write a program to subtract BCD data at 4286H form BCD data at 4284H data at 4287H and store the BCD result at 4288H
         ;; loading data
-        STA     4584H
+        LDA     4584H
         MOV     C, A
-        STA     4586H
+        LDA     4586H
         SUB     C
         ;; converting to BCD
         DAA
         ;; storing result
-        LDA     4588H
+        STA     4588H
         ;; loading data
-        STA     4585H
+        LDA     4585H
         MOV     B, A
         STA     4587H
         SBB     B
         ;; converting to BCD
         DAA
         ; storing result
-        LDA     4589H
+        STA     4589H
         HLT

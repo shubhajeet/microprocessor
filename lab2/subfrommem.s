f7af17cd0aa0b75b1ac2e4dfc7e4cb4a22f3b8e5
sujit <mail@maharjansujit.com.np>
2015-07-21 11:02:12 +0545

    stashing changes

--- lab2/subfrommem.s
+++ lab2/subfrommem.s
@@ -4,28 +4,27 @@
         XCHG
         LHLD    8092H
         MOV     A, L
-        MOV     B, E
-	MVI	C, 00h
-        CALL BCDsub
-        MOV L, A
-        MOV A, H
-        MOV B, D
-        CALL BCDsub
-        MOV H, A
+        CALL    ncmp
+        ADD     E
+	DAA
+        JC      l1
+                CALL ncmp
+l1:     MOV     L,A
+	CMC        
+	MOV     A,H
+	JNC	l3
+	ADI	01H
+l3: 	CALL    ncmp
+        ADD     D
+	DAA
+        JC      l2
+        CALL ncmp
+        CMC
+l2:     MOV     H,A
         SHLD     8094H
         HLT
-BCDsub: ADD C
-	CALL    ncmp
-        ADD     E
-        DAA
-        JC     l1
-        call ncmp
-	MVI	C,01H
-l1:     RET
-
 ncmp:   MOV     C,A
         MVI     A,99H
-        SUB     C
+        SBB     C
         INR     A
-	MVI	C,00h
         RET

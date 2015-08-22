271c7a794ca759b202ca2d6df5e100a8f3ae56eb
sujit <mail@maharjansujit.com.np>
2015-08-22 09:41:08 +0545

    division by shifting

--- lab5/optional/division.s
+++ lab5/optional/division.s
@@ -1,7 +1,35 @@
         ;; Write a program for 8085 microprocessor to read dividend and divisor from memory address 8000H and 8001H respectively and store the quotient and reminder to memory address 8002H and 8003H respectively using division by shifting.
         ;; reading operand
         LXI H, 8000H
-        MOV A, M
+        MOV D, M
         INX H
-        MOV B, M
+        MOV E, M
+
+        ;; writing data
+        MVI C, 08H
+        MVI B, 00H
+loop:   PUSH PSW
+        MOV A, D
+        RAL
+        MOV D, A
+        POP PSW
+        RAR
+        CMP E
+        JNC div
+        SUB E
+
+div:    PUSH PSW
+        MOV A, B
+        RAL
+        POP PSW
+
+        DCR C
+        JNZ loop
+
+        ;; storing data
+        INX H
+        MOV M, B
+        INX H
+        MOV M, A
+
         RST 5

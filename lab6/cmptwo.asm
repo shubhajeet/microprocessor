f52c5ea93d01076642c8b7fb665f0604b1785cba
sujit <mail@maharjansujit.com.np>
2015-08-25 02:46:45 +0545

    initial program

---
+++ lab6/cmptwo.asm
@@ -0,0 +1,24 @@
+        TITLE two tables having ten 16 bit data in each and Wap to add two numbers and store in 3rd
+        .MODEL SMALL
+        .STACK
+        .DATA
+        VALS1 DW 100H 200H 300H 500H 600H 700H 800H 900H 1000H
+        VALS2 DW 100H 200H 300H 500H 600H 700H 800H 900H 1000H
+        VALS3 DW 10 DUP(?)
+        .CODE
+        MAIN PROC FAR
+        MOV AX, @DATA
+        MOV DS, AX
+
+        MOV CX, 10
+        MOV DI, 10
+lo:     MOV AX, VALS1[DI]
+        CMP VALS2[DI]           ; TODO
+        MOV VALS3[DI], AX
+        loop lo
+
+        MOV AX, 4C00H
+        INT 21H
+
+        MAIN ENDP
+        END MAIN

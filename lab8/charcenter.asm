beffcb1dd3cc6c3f58abde172c6b4cf84b621987
sujit <mail@maharjansujit.com.np>
2015-09-01 18:18:28 +0545

    dispalying char at its center

--- lab8/charcenter.asm
+++ lab8/charcenter.asm
@@ -1,35 +1,49 @@
-        TITLE display char in center
         .MODEL SMALL
         .DATA
         STRLEN DB 24
         STRSZ DB ?
-        STR 60 DB DUP('$')
+        STR DB 24 DUP('$')
         STRTRM DB '$'
         .STACK
         .CODE
+
         MAIN PROC FAR
+
         MOV AX, @DATA
         MOV DS, AX
-        ;; clearing the screen
-        MOV AX, 00H
-        INT 10H
+
         ;; reading string
         LEA DX, STRLEN
         MOV AH, 0AH
         INT 21H
+
         ;; clearing the screen
-        MOV AX, 00H
-        MOV AH, 13H
+        MOV AH, 00H
+        MOV AL, 03H
         INT 10H
+
         ;; displaying in the center
         ;; setting cursor position
+        LEA SI, STR
+        MOV CH, 00
+        MOV CL, STRSZ
+        MOV DX, 00H
+L1:     MOV BH, 00H
+        MOV DL, 40
+        MOV AH, 02H
         INT 10H
         ;; displaying in cursor position
-        LEA DX, STR
-        MOV AH, 09H
+        MOV DL, [SI]
+        MOV AH, 02H
         INT 21H
+        INC DH
+        INC SI
+        loop l1
+
         ;; ending the program
         MOV AX, 4c00H
         INT 21H
+
         MAIN ENDP
+
         END MAIN

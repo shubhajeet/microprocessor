8100547f9bd31975ed3dded5fb3044332d11548c
Sujit Maharjan <mail@maharjansujit.com.np>
2015-09-01 08:18:44 +0545

    initial program

--- lab8/charcenter.asm
+++ lab8/charcenter.asm
@@ -1,11 +1,33 @@
-        TITLE
+        TITLE display char in center
         .MODEL SMALL
         .DATA
+        STRLEN DB 60
+        STRSZ DB ?
+        STR 60 DB DUP(' ')
+        STRTRM DB '$'
         .STACK
         .CODE
         MAIN PROC FAR
         MOV AX, @DATA
         MOV DS, AX
+        ;; clearing the screen
+        MOV AX, 00H
+        INT 10H
+        ;; reading string
+        LEA DX, STRLEN
+        MOV AH, 0AH
+        INT 21H
+        ;; clearing the screen
+        MOV AX, 00H
+        MOV AH, 13H
+        INT 10H
+        ;; displaying in the center
+        ;; setting cursor position
+        INT 10H
+        ;; displaying in cursor position
+        LEA DX, STR
+        MOV AH, 09H
+        INT 21H
         ;; ending the program
         MOV AX, 4c00H
         INT 21H
--- lab8/scroll.asm
+++ lab8/scroll.asm
@@ -1,11 +1,31 @@
-        TITLE
+        TITLE scroll
         .MODEL SMALL
         .DATA
+        STR DB "Programming in Assembly Language is Fun",'$'
         .STACK
         .CODE
         MAIN PROC FAR
         MOV AX, @DATA
         MOV DS, AX
+        ;; setting reverse video mode
+        MOV AH, 00
+        MOV AL, 0EH
+        INT 10H
+        ;; moving to row 5, 20 column
+        MOV DH, 5                  ;row
+        MOV DL, 20                 ;column
+        MOV AH, 02H
+        INT 10H
+        ;; scrolling to 20 row, column 60
+	MOV DH, 20                  ;row
+	MOV DL, 60                 ;column
+        MOV AH, 07H
+        INT 10H
+
+        ;; displaying the string
+        LEA DX, str
+        MOV AH, 09H
+        INT 21H
         ;; ending the program
         MOV AX, 4c00H
         INT 21H

f7a1d2f555adb93912cc5470b25b15b32523c9e6
sujit <mail@maharjansujit.com.np>
2015-09-03 22:26:33 +0545

    diagnoal working

--- lab8/diagnoal.asm
+++ lab8/diagnoal.asm
@@ -11,27 +11,37 @@
         MOV AX, @DATA
         MOV DS, AX
         ;; reading from the user
-
         LEA DX, STRLEN
         MOV AH, 0AH
         INT 21H
+        ;; clearing the screen
+        MOV AH, 00H
+        MOV AL, 03H
+        INT 10H
         ;; displaying string
-        MOV CX, 00
         MOV CL, STRSZ
         LEA BX, STR
 l1:     MOV DL, [BX]
         CMP DL, ' '
         JNE ex
         ;; getting present cursor position
+        PUSH CX
+        PUSH DX
         MOV AH, 03H
         INT 10H
-        ;; moving cursor position down by one row
-        INC DH
+        ;; setting cursor position
+        ADD DH, 1
         MOV AH, 02H
         INT 10H
-ex:     MOV AH, 02H
+        POP DX
+        POP CX
+        JMP exit
+        ;; displaying the character
+ex:     MOV DL, [BX]
+        MOV AH, 02H
         INT 21H
-        INC BX
+exit:   INC BX
+        INC DL
         loop l1
         ;; ending the program
         MOV AX, 4c00H
